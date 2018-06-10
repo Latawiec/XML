cc8363907a685568e6fdc68407e81be7f05185f7
Lukasz Nadarzynski <lukasz.nadarzynski@gmail.com>
2018-06-10 21:20:44 +0200

    minor changes

--- XMLtoObj/XMLtoObj/Models/ReleaseDate.cs
+++ XMLtoObj/XMLtoObj/Models/ReleaseDate.cs
@@ -7,47 +7,12 @@ using System.Text;
 
 namespace XMLtoObj.Models
 {
-    public class ReleaseDate : INotifyPropertyChanged
+    public class ReleaseDate
     {
         public string Day { get; set; }
         public string Month { get; set; }
         public string Year { get; set; }
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class ReleaseDateReader : INotifyPropertyChanged
-    {
-        ObservableCollection<ReleaseDate> _ReleaseDates;
-
-        public ObservableCollection<ReleaseDate> ReleaseDates
-        {
-            get { return _ReleaseDates; }
-            private set
-            {
-                _ReleaseDates = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public ReleaseDateReader()
-        {
-            ReleaseDates = new ObservableCollection<ReleaseDate>();
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-    }
+    } 
+   
 }
